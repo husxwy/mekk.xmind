a06b18b3fdf8f525c0ccb0b61c85488e94db0431
Marcin Kasperski <Marcin.Kasperski@mekk.waw.pl>
2008-12-13 14:32:15 +0100

    porz?dki

--- tests/test_xmindwrite.py
+++ tests/test_xmindwrite.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-import unittest, zipfile, tempfile, os
-from mekk.xmind import XMindDocument
+import unittest, zipfile, tempfile, os, re
+from sample_maps import *
 
 class MapCompareBase(object):
     """
@@ -46,28 +46,5 @@ class MapCompareBase(object):
 class SimpleMapTestCase(MapCompareBase, unittest.TestCase):
     pattern_file = "simple.xmind"
     def generate(self):
-        doc = XMindDocument.create(u"Główny", u"Projekty")
-        sheet = doc.get_first_sheet()
-        root = sheet.get_root_topic()
-        root.set_note("View the Help sheet for info\nwhat you can do with this map")
-
-        style = doc.create_topic_style(fill = "#37D02B")
-        #style_sub = doc.create_topic_style(fill = "CCCCCC")
-
-        for i in range(1,5):
-            topic = root.add_subtopic(u"Elemiątko %d" % i)
-            topic.set_label("%d" % i)
-            topic.set_style(style)
-            topic.set_link("http://info.onet.pl")
-            for j in range(1,3):
-                subtopic = topic.add_subtopic(u"Subelemiątko %d/%d" % (i,j))
-                subtopic.add_marker("task-start")
-                if j < 2:
-                    subtopic.add_marker("other-people")
-
-        legend = sheet.get_legend()
-        legend.add_marker("task-start", u"Dzień dobry")
-        legend.add_marker("other-people", u"Do widzenia")
-
-        return doc
+        return generate_simple()
 
