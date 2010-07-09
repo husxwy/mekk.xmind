59520767fa57c5a81f11952c4aaea65dc72aab74
Marcin Kasperski <Marcin.Kasperski@mekk.waw.pl>
2010-07-10 00:27:38 +0200

    Tests more files

--- examples/map_parser.py
+++ examples/map_parser.py
@@ -1,22 +1,27 @@
 # -*- coding: utf-8 -*-
-# (c) 2008, Marcin Kasperski
+# (c) 2008-2010, Marcin Kasperski
 
 from mekk.xmind import XMindDocument
 
-xmind = XMindDocument.open("test.xmind")
+def parse_and_print(file_name):
+    xmind = XMindDocument.open(file_name)
 
-sheet = xmind.get_first_sheet()
-print "Sheet title: ", sheet.get_title()
+    sheet = xmind.get_first_sheet()
+    print "Sheet title: ", sheet.get_title()
 
-root = sheet.get_root_topic()
-print "Root title: ", root.get_title()
-print "Root note: ", root.get_note()
+    root = sheet.get_root_topic()
+    print "Root title: ", root.get_title()
+    print "Root note: ", root.get_note()
 
-for topic in root.get_subtopics():
-    print "* ", topic.get_title()
-    print "   label: ", topic.get_label()
-    print "   link: ", topic.get_link()
-    print "   markers: ", list(topic.get_markers())
-    # topic.get_subtopics()
+    for topic in root.get_subtopics():
+        print "* ", topic.get_title()
+        print "   label: ", topic.get_label()
+        print "   link: ", topic.get_link()
+        print "   markers: ", list(topic.get_markers())
+        # topic.get_subtopics()
 
-legend = sheet.get_legend()
+    #legend = sheet.get_legend()
+
+if __name__ == "__main__":
+    parse_and_print("test.xmind")
+    parse_and_print("test_manual.xmind")
