5cdd9328aadd83df1463461d3dfd2467ff7cac2a
Marcin Kasperski <Marcin.Kasperski@mekk.waw.pl>
2010-12-16 22:39:25 +0100

    Slightly better example

--- examples/miscellaneous/mlhim/xmindparse.py
+++ examples/miscellaneous/mlhim/xmindparse.py
@@ -9,14 +9,9 @@ print "CCD Title:       ", ccd.get_title()
 print "CCD Template Id: ", ccd.get_correlation_id()
 # here we need to compare the id to know which CCD template is being used.
 
-for cl in ccd.get_subtopics():  #current level
-    if cl.get_title() == 'definition':
-        x = True
-        while x:
-            nl = cl.get_suptopics() # next level generator
-            for cl in nl:
-                print cl.get_title()
-                if cl is None:
-                    x = False
-            
-    
+for top_item in ccd.get_subtopics():  #current level
+    if top_item.get_title() == 'definition':
+        for level2item in top_item.get_subtopics(): # next level generator
+            print "* ", level2item.get_title()
+            for level3item in level2item.get_subtopics():
+                print "** ", level3item.get_title()
